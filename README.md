8ad9d25675abc9d5ab363b1097be900a6b7565cb
Lucius Nelson <lnelson@pixelmedia.com>
2021-06-29 10:29:50 -0700

    adds initial build

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # xmlparse-pd
 XML parsing for meta data, aimed at page designer components
+
+## About Page Designer Meta Data Management
+Within the SFCC framework, Page Designer pages are content assets that contain child content assets.
+
+It is a massive time suck whenever you need to manage these meta data files as you currently need to parse these files manually.
+
+... enter some script I've built
+
+## How to use
+example usage:
+`npm run parsexml homepage-example '/sample.xml' '/parsed'`
+
+This node script accepts three parameters
+1. Page Designer page level id
+2. Read path, ie. the XML to parse
+3. Write path, ie. where you want the parsed XML
+
+NOTE: the paths are relative to index.js
---
+++ index.js
@@ -0,0 +1,61 @@
+/* eslint no-console: 0 */
+var parser = require('xml2json');
+var myArgs = process.argv.slice(2);
+var fs = require('fs');
+
+var readPath = myArgs[1];
+var writePath = myArgs[2];
+if (!readPath) {
+    console.error('Read path not provided; please provide path to your xml.');
+    process.exit(1);
+}
+if (!writePath) {
+    console.error('Write path not provided; please provide path for your parsed xml.');
+    process.exit(1);
+}
+
+var xml = fs.readFileSync(__dirname + readPath, 'utf8');
+var json = parser.toJson(xml, { object: true, reversible: true });
+var contentCollection = [];
+var content = json && json.library && json.library.content;
+var xmlns = json.library.xmlns;
+var headtag = `<?xml version="1.0" encoding="UTF-8"?>`;
+var libtag = `</library>`;
+var xmlStructure = { library: {} };
+var collector = function (id) {
+    content.forEach(function (el) {
+        // grab the content that matches the id passed
+        if (el['content-id'] && el['content-id'] === id) {
+            contentCollection.push({ content: el });
+
+            // grab the related children
+            if (el['content-links'] && el['content-links']['content-link'] && el['content-links']['content-link'].length) {
+                var childContent = el['content-links']['content-link'];
+                childContent.forEach(function (c) {
+                    var cid = c['content-id'];
+                    collector(cid);
+                });
+            }
+        }
+    });
+};
+if (!content) {
+    console.error('XML Content inaccessible. Please check structure.');
+    process.exit(1);
+} else {
+    collector(myArgs[0]);
+}
+xmlStructure.library.xmlns = xmlns;
+xmlStructure.library.content = contentCollection;
+
+var parsedXml = parser.toXml(JSON.stringify(xmlStructure));
+parsedXml = headtag + parsedXml + libtag;
+
+fs.writeFile(__dirname + writePath, parsedXml, function (err) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log('file successfully written at ' + __dirname + writePath);
+});
